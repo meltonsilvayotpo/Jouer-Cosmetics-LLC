f3681efd3908edf410c7408be340655fb688e5dc
meltonsilvayotpo <melton.silva@yotpo.com>
2025-05-03 04:37:14 +0800

    Update App.js
    update product ID

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "2208269631550";
+  let ProductId = "6140791128241";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
